e2db83e12dfa358f9085cae7f9d841bb9dd2d222
hnagamin <1995.hnagamin@gmail.com>
2014-10-19 23:51:57 +0900

    Yoを送信したときに一瞬Send Yo!と表示するようにした
    tk-callでTclの組み込みメソッドafterを呼んでいる。

--- tk.scm
+++ tk.scm
@@ -4,15 +4,16 @@
 (tk-init '())
 (let1 friends (get-friends)
   (dolist (friend friends)
-    (let1 path (string->symbol (string-append ".yo" friend))
+    (let1 path (string->symbol (string-append ".yoto" friend))
       (tk-button path 
                  :text friend
                  :command (lambda () 
                             (send-yo friend)
                              (tk-call path 'configure
                                       :text "Send Yo!")
-                             (sys-sleep 1)
-                             (tk-call path 'configure
-                                      :text friend)))
+                             (tk-call 
+                               'after 800
+                               (format "~A configure -text ~A"
+                                       path friend))))
       (tk-pack path :fill 'x))))
 (tk-mainloop)
