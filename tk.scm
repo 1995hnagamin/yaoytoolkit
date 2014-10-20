eb4fd0397be511c34c3bb234c4bc98c6daa872bd
hnagamin <1995.hnagamin@gmail.com>
2014-10-20 18:33:22 +0900

    add yo-button-symbol, tk-after
    新たに実装
    yo-button-symbol friend
      friendへYoを送信するボタンへのpathを表すシンボルを返す
    
    <macro> tk-after ms command args ...
      このマクロは以下の形式に展開されます。
      (tk-call 'after ms (format command args ...))

--- tk.scm
+++ tk.scm
@@ -1,18 +1,25 @@
 (use tk)
 (load "./yaoy.scm")
 
+(define (yo-button-symbol friend)
+  (string->symbol (string-append ".yoto" friend)))
+
+(define-syntax tk-after
+  (syntax-rules ()
+    [(_ ms command args ...)
+     (tk-call 'after ms (format command args ...))]))
+
 (tk-init '())
 (let1 friends (get-friends)
   (dolist (friend friends)
-    (let1 path (string->symbol (string-append ".yoto" friend))
+    (let1 path (yo-button-symbol friend)
       (tk-button
         path 
         :text friend
         :command (lambda () 
                    (let1 result (send-yo friend)
                      (tk-call path 'configure :text result)
-                     (tk-call 'after 1000
-                              (format "~A configure -text ~A"
-                                      path friend)))))
+                     (tk-after 800 "~A configure -text ~A"
+                               path friend))))
       (tk-pack path :fill 'x))))
 (tk-mainloop)
