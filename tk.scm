082517a8ea8515eba9f5955647d8ff4f63052780
hnagamin <1995.hnagamin@gmail.com>
2014-10-20 18:40:00 +0900

    mainを追加

--- tk.scm
+++ tk.scm
@@ -9,17 +9,21 @@
     [(_ ms command args ...)
      (tk-call 'after ms (format command args ...))]))
 
-(tk-init '())
-(let1 friends (get-friends)
-  (dolist (friend friends)
-    (let1 path (yo-button-symbol friend)
-      (tk-button
-        path 
-        :text friend
-        :command (lambda () 
-                   (let1 result (send-yo friend)
-                     (tk-call path 'configure :text result)
-                     (tk-after 800 "~A configure -text ~A"
-                               path friend))))
-      (tk-pack path :fill 'x))))
-(tk-mainloop)
+(define (pack-yo-buttons)
+  (let1 friends (get-friends)
+    (dolist (friend friends)
+      (let1 path (yo-button-symbol friend)
+        (tk-button
+          path 
+          :text friend
+          :command (lambda () 
+                     (let1 result (send-yo friend)
+                       (tk-call path 'configure :text result)
+                       (tk-after 800 "~A configure -text ~A"
+                                 path friend))))
+        (tk-pack path :fill 'x)))))
+
+(define (main args)
+  (tk-init '())
+  (pack-yo-buttons)
+  (tk-mainloop))
